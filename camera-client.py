5124e92e58962915ea0871a6e7dfcf68c0d5c1aa
ist192474 <guillermo.bettencourt@tecnico.ulisboa.pt>
2023-03-21 18:09:58 +0000

    Add camera sensor client app

---
+++ camera-client.py
@@ -0,0 +1,30 @@
+from gpiozero import MotionSensor
+
+import io
+import picamera
+import requests
+import cv2
+import numpy as np
+import time
+
+address = 'http://localhost:5000/intruder_detected'
+camera_url = 'http://192.168.148.46:8080/video'
+
+cap = cv2.VideoCapture(camera_url)
+
+pir = MotionSensor(12)
+i = 0
+
+while True:
+    pir.wait_for_motion()
+    print("Motion detected.")
+
+    filename = "/home/pi/Desktop/code/motion_captured" + str(i) + ".jpg"
+
+    ret,frame = cap.read()
+    cv2.imwrite(filename, frame)
+    with open(filename, 'rb') as photo:
+        response = requests.post(address, files={'image': photo})
+        print(response.json())
+    i = i + 1
+    pir.wait_for_no_motion()
