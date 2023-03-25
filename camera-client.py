0aa543da7a2ad7651b051a462acef59d7fc08f7a
rodrigo1110 <rodrigogoma007@gmail.com>
2023-03-25 17:15:45 +0000

    camera client updated before

--- camera-client.py
+++ camera-client.py
@@ -8,9 +8,7 @@ import numpy as np
 import time
 
 address = 'http://localhost:5000/intruder_detected'
-camera_url = 'http://192.168.148.46:8080/video'
-
-cap = cv2.VideoCapture(camera_url)
+camera_url = 'http://192.168.139.99:4747/video'
 
 pir = MotionSensor(12)
 i = 0
@@ -21,10 +19,14 @@ while True:
 
     filename = "/home/pi/Desktop/code/motion_captured" + str(i) + ".jpg"
 
+    cap = cv2.VideoCapture(camera_url)
     ret,frame = cap.read()
     cv2.imwrite(filename, frame)
+
     with open(filename, 'rb') as photo:
         response = requests.post(address, files={'image': photo})
         print(response.json())
+        
     i = i + 1
     pir.wait_for_no_motion()
+    cap.release()
