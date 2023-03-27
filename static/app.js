78c2f50a250a90d0d5f99b18957d10cc90cf0e8f
ist192474 <guillermo.bettencourt@tecnico.ulisboa.pt>
2023-03-27 14:38:54 +0100

    Fix errors and refactor

--- static/app.js
+++ static/app.js
@@ -5,33 +5,33 @@ fetch('/get_th_data')
     document.getElementById('humidity').innerText = data.humidity;
   });
 
-//Get the image history
 fetch('/get_images')
   .then(response => response.json())
   .then(data => {
     const imageList = document.getElementById('image-list');
     data.images.forEach(image => {
       const listItem = document.createElement('li');
+      
       listItem.className = 'image-item';
       const img = document.createElement('img');
       img.src = `data:image/png;base64,${image.data}`;
-      img.style.display = 'block'; // center the image
-      img.style.margin = '0 auto'; // center the image
-      img.style.width = '50%'; // set a specific size for the image
-      
+      img.style.display = 'block'; 
+      img.style.margin = '0 auto'; 
+      img.style.width = '50%'; 
       imageDate = image.name.replace('.jpg', '');
+
       const text = document.createTextNode(imageDate);
       const textElement = document.createElement('p');
-      textElement.style.textAlign = 'center'; // center the text
+      textElement.style.textAlign = 'center'; 
       textElement.appendChild(text);
 
-      listItem.appendChild(textElement); // add text above the image
+      listItem.appendChild(textElement); 
       listItem.appendChild(img);
+
       imageList.appendChild(listItem);
     });
   });
 
-  // Get the security status
   const securityToggle = document.getElementById("security-toggle");
   securityToggle.innerHTML = "Enabled";
   securityToggle.style.backgroundColor = "#27ae60";
@@ -39,12 +39,7 @@ fetch('/get_images')
     fetch('/enable_security')
       .then(response => response.json())
       .then(data => {
-        // Find the security toggle button
-
-        // Toggle the security status
         let securityIsEnabled = Boolean(data.isEnabled);
-        
-        // Update the security toggle button text and color
         if (securityIsEnabled) {
           securityToggle.innerHTML = "Enabled";
           securityToggle.style.backgroundColor = "#27ae60";
--- temperature_humidity_to_json.py
+++ temperature_humidity_to_json.py
@@ -1,20 +1,16 @@
 import json
 from datetime import datetime
 
-# set the filename and path of the JSON file
 filename = 'static/data.json'
 filepath = f'/mnt/c/Users/guill/OneDrive/Escritorio/E.I/MEIC/S4/AI/Project/Smart-Attic/{filename}'
 
-# function to add new metric to the JSON file
 def add_metric(temperature, humidity):
-    # read the existing JSON data from the file
     try:
         with open(filepath, 'r') as file:
             data = json.load(file)
     except FileNotFoundError:
         data = []
 
-    # create a new metric object with the provided data
     new_temperature = {
         'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'metric': 'Temperature',
@@ -27,10 +23,8 @@ def add_metric(temperature, humidity):
         'value': humidity
     }
 
-    # append the new metric to the existing data
     data.append(new_temperature)
     data.append(new_humidity)
-
-    # write the updated data back to the JSON file
+    
     with open(filepath, 'w') as file:
         json.dump(data, file, indent=2)
